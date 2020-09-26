7c85786d168ba29a0da5da04cbdb91febe96f4e8
Vyzytor <59753939+Vyzytor@users.noreply.github.com>
2020-09-26 18:22:42 -0500

    Update Fib.py

--- Fib.py
+++ Fib.py
@@ -8,17 +8,17 @@ def Fib(Number):
     i = 0
     First_Value = 0
     Second_Value = 1
-           
+
     # Find & Displaying Fibonacci series
     while(i < Number):
-           if(i <= 1):
-                      Next = i
-           else:
-                      Next = First_Value + Second_Value
-                      First_Value = Second_Value
-                      Second_Value = Next
-           print(Next)
-           i = i + 1
+       if(i <= 1):
+           Next = i
+        else:
+           Next = First_Value + Second_Value
+           First_Value = Second_Value
+           Second_Value = Next
+      print(Next)
+      i = i + 1
             
             
 Fib(3)
