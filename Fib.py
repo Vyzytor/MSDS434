47fdbf9df6b716b6ca480174bde04c9c3b6502fe
Vyzytor <59753939+Vyzytor@users.noreply.github.com>
2020-09-27 13:48:56 -0500

    Update Fib.py

--- Fib.py
+++ Fib.py
@@ -4,14 +4,14 @@
 # Number = int(input("\nPlease Enter the Range Number: "))
 
 # Initializing First and Second Values of a Series
-def Fib(Number):
+def Fib(Number:
     i = 0
     First_Value = 0
     Second_Value = 1
 
     # Find & Displaying Fibonacci series
     while(i < Number):
-    if(i <= 1):
+        if(i <= 1):
             Next = i
         else:
             Next = First_Value + Second_Value
